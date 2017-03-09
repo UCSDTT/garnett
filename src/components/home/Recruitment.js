adef73f6583d91d3735543b9d4e8bae0b43e8ab6
Daniel Zhang <daniel_zhang@intuit.com>
2017-03-08 16:51:51 -0800

    Test commit + ignoring .idea files in git

--- src/components/home/Recruitment.js
+++ src/components/home/Recruitment.js
@@ -3,8 +3,8 @@ import React, {Component} from 'react';
 class Recruitment extends Component {
   render() {
     return (
-      <div className="photos">
-        <p>This is the Recruitment Module</p>
+      <div className="recruitment">
+        <p>Why Rush</p>
       </div>
     );
   }
